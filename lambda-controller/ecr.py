73d795e6dc2ce6e0bcb8f2f87f24c05c9a4f5c2d
azubuike <azubuikeokom@gmail.com>
2025-07-15 09:46:05 -0500

    New changes

--- lambda-controller/ecr.py
+++ lambda-controller/ecr.py
@@ -1,9 +1,11 @@
 class ECR:
+    """Take in ECR Image Update event to return Image Repo properties"""
     def __init__(self,event):
         self.event = event
 
-    def getImageURI(self) -> str:
-        ecr_repo = event['detail']['repository-name']
-        image_tag = event['detail']['image-tag']
+    def getImageURIAndRepoName(self) -> str:
+        ecr_repo = self.event['detail']['repository-name']
+        image_tag = self.event['detail']['image-tag']
         image_uri = f"{ecr_repo}:{image_tag}"
-        return image_uri
+        return (image_uri,ecr_repo)
+
--- lambda-controller/main.py
+++ lambda-controller/main.py
@@ -3,16 +3,17 @@ from dotenv import load_dotenv
 import ecr
 import os
 
-load_dotenv()
+
 
 def lambda_handler(event, context):
     """"Process imcoming ECR image upload event"""
     my_ecr = ecr.ECR(event)
-    image_uri = my_ecr.getImageURI()
+    image_uri, repo_name = my_ecr.getImageURIAndRepoName()
     REPO_URL = os.getenv("REPO_URL")
     repo = Repo(REPO_URL)
+    project = repo.modifyProjectName(repo_name)
     repo.clone()
-    repo.updateProjectVariables(image_uri)
+    repo.updateProjectVariables(project,image_uri)
 
-    
+
 
